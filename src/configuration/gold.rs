7bfe568369c56123bcbf580fea5c0a2969bab21c
Brian Strauch <bstrauch@confluent.io>
2023-06-30 14:16:40 -0700

    configuration file help text

--- src/configuration/gold.rs
+++ src/configuration/gold.rs
@@ -1,33 +1,31 @@
 #![allow(non_snake_case)]
 
 use serde::Deserialize;
+use serde_yaml::Value;
 
 use super::golangci::GolangciConfiguration;
 
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Configuration {
-    pub G0000: Option<Empty>,
+    pub G0000: Option<Value>,
     pub G0001: Option<Vec<String>>,
-    pub SA1000: Option<Empty>,
+    pub SA1000: Option<Value>,
 }
 
 impl Configuration {
-    pub fn new() -> Self {
+    pub fn default() -> Self {
         Self {
-            G0000: Some(Empty {}),
+            G0000: Some(Value::Null),
             G0001: Some(vec![String::from("standard"), String::from("default")]),
-            SA1000: Some(Empty {}),
+            SA1000: Some(Value::Null),
         }
     }
 
     pub fn from(golangci_configuration: GolangciConfiguration) -> Self {
-        let mut configuration = Self::new();
+        let mut configuration = Self::default();
 
         configuration.G0001 = Some(golangci_configuration.linters_settings.gci.sections);
 
         configuration
     }
 }
-
-#[derive(Debug, Deserialize)]
-pub struct Empty {}
--- src/module_linter.rs
+++ src/module_linter.rs
@@ -2,7 +2,7 @@ use crate::{
     configuration::{golangci::GolangciConfiguration, gold::Configuration},
     file_linter::FileLinter,
 };
-use std::path::Path;
+use std::{fs::File, path::Path};
 use walkdir::WalkDir;
 
 pub struct ModuleLinter {
@@ -12,17 +12,20 @@ pub struct ModuleLinter {
 impl ModuleLinter {
     pub fn new() -> Self {
         Self {
-            configuration: Configuration::new(),
+            configuration: Configuration::default(),
         }
     }
 
     pub fn run(mut self, path: &str) -> bool {
-        if let Ok(file) = std::fs::File::open(Path::new(path).join(".gold.yml")) {
+        if let Ok(file) = File::open(Path::new(path).join(".gold.yml")) {
             self.configuration = serde_yaml::from_reader(&file).unwrap();
-        } else if let Ok(file) = std::fs::File::open(Path::new(path).join(".golangci.yml")) {
+        } else if let Ok(file) = File::open(Path::new(path).join(".golangci.yml")) {
+            eprintln!("Using configuration from .golangci.yml");
             let golangci_configuration: GolangciConfiguration =
                 serde_yaml::from_reader(&file).unwrap();
             self.configuration = Configuration::from(golangci_configuration);
+        } else {
+            eprintln!("Could not find .gold.yml, using default configuration");
         }
 
         let mut exit = false;
