b7c23324cf5f15a6711313372d8f32a40730171b
Brian Strauch <bstrauch@confluent.io>
2023-07-14 19:27:59 -0700

    move from lib to main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-use gold::lint;
-use std::{env, process::ExitCode};
+#[macro_use]
+extern crate lazy_static;
+
+mod configuration;
+mod error;
+mod file_linter;
+mod go;
+mod module_linter;
+mod query;
+mod rules;
+
+use file_linter::FileLinter;
+use module_linter::ModuleLinter;
+use std::{env, fs, io, process::ExitCode};
+use tree_sitter::Language;
+
+extern "C" {
+    fn tree_sitter_go() -> Language;
+}
 
 fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
@@ -16,5 +33,16 @@ fn main() -> ExitCode {
             eprintln!("Error: {}", e);
             ExitCode::FAILURE
         }
+    }
+}
+
+pub fn lint(path: &str) -> io::Result<bool> {
+    let module_linter = ModuleLinter::new();
+
+    if fs::metadata(path)?.is_dir() {
+        Ok(module_linter.run(path))
+    } else {
+        let mut linter = FileLinter::new(&module_linter, path.to_string());
+        Ok(linter.run())
     }
 }
