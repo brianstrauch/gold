5a8d28b47757c38c8b66b5532c3309141091526d
Brian Strauch <bstrauch@confluent.io>
2023-07-30 16:14:07 -0700

    git add

---
+++ tests/F0000/1_test.go
@@ -0,0 +1,5 @@
+package F0000_test
+
+func f1(a string, b string)                   {}
+func f2(a int, b string, c string, d *string) {}
+func f3(a, b string, c, d string)             {}
---
+++ tests/F0001/1_test.go
@@ -0,0 +1,8 @@
+package F0001_test
+
+import (
+	"github.com/brianstrauch/gold/tests/F0000"
+
+	"fmt"
+	"github.com/spf13/pflag"
+)
