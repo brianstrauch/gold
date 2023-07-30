5dd2c5e60fe669689f09220bab086f8fca5455dc
Brian Strauch <bstrauch@confluent.io>
2023-07-30 16:32:12 -0700

    add adversarial test to F0000

--- tests/F0000/1.go
+++ tests/F0000/1.go
@@ -1,5 +1,5 @@
 package F0000
 
-func f1(a, b string)                   {}
+func f1(a, b int)                      {}
 func f2(a int, b, c string, d *string) {}
 func f3(a, b, c, d string)             {}
--- tests/F0000/1_test.go
+++ tests/F0000/1_test.go
@@ -1,5 +1,5 @@
 package F0000_test
 
-func f1(a string, b string)                   {}
+func f1(a int, b int)                         {}
 func f2(a int, b string, c string, d *string) {}
 func f3(a, b string, c, d string)             {}
